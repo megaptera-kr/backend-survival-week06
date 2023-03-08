6d39bb630555a671f8e64ad0085f205c1501c231
hsw0905 <hsw0905@gmail.com>
2023-03-08 21:31:37 +0900

    AppRunner : 테이블 생성 sql

--- src/main/java/kr/megaptera/jdbc/assignment/AppRunner.java
+++ src/main/java/kr/megaptera/jdbc/assignment/AppRunner.java
@@ -1,10 +1,34 @@
 package kr.megaptera.jdbc.assignment;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AppRunner implements CommandLineRunner {
+    private final JdbcTemplate jdbcTemplate;
+
+    public AppRunner(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public void run(String... args) {
-        // TODO: posts 와 comments 테이블을 생성해 주세요.
+        String sql = """
+                CREATE TABLE IF NOT EXISTS posts (
+                    id VARCHAR(30),
+                    title VARCHAR(50),
+                    author VARCHAR(30),
+                    content VARCHAR(200)
+                );
+                
+                CREATE TABLE IF NOT EXISTS comments (
+                    id VARCHAR(30),
+                    post_id VARCHAR(30),
+                    author VARCHAR(30),
+                    content VARCHAR(200)
+                );
+                """;
+        jdbcTemplate.execute(sql);
     }
 }
