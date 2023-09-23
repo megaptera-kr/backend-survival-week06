e3d966c67c8e80602db075bda1490dab38b21e42
jinhea <nordstromm03@gmail.com>
2023-09-23 18:52:05 +0900

    기본 설정

--- src/main/java/kr/megaptera/jdbc/assignment/Application.java
+++ src/main/java/kr/megaptera/jdbc/assignment/Application.java
@@ -2,12 +2,27 @@ package kr.megaptera.jdbc.assignment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer webMvcConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry
+						.addMapping("/**")
+						.allowedMethods("GET","POST","PUT","DELETE","PATCH","OPTIONS");
+			}
+		};
 	}
 
 }
