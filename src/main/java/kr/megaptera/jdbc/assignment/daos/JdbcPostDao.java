c77be05bc37e0f654f0259dbb38f8739532614b8
hoeseong <hoeseong19@naver.com>
2023-10-29 17:31:49 +0900

    JdbcPostDao - save 함수 구현

--- src/main/java/kr/megaptera/jdbc/assignment/daos/JdbcPostDao.java
+++ src/main/java/kr/megaptera/jdbc/assignment/daos/JdbcPostDao.java
@@ -1,4 +1,42 @@
 package kr.megaptera.jdbc.assignment.daos;
 
+import kr.megaptera.jdbc.assignment.models.posts.Post;
+import kr.megaptera.jdbc.assignment.models.posts.PostId;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Component;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+@Component
 public class JdbcPostDao implements PostDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    private final String POST_TABLE_NAME = "post";
+
+    public JdbcPostDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void save(Post post) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        String sql = """
+                INSERT INTO %s (title, author, content) VALUES (?, ?, ?);
+                """.formatted(POST_TABLE_NAME);
+
+        jdbcTemplate.update(con -> {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, post.getTitle());
+            ps.setString(2, post.getAuthor());
+            ps.setString(3, post.getContent());
+
+            return ps;
+        }, keyHolder);
+
+        // db에서 생성된 id를 postId에 저장
+        post.setId(new PostId(keyHolder.getKeys().get("id").toString()));
+    }
 }
--- src/main/java/kr/megaptera/jdbc/assignment/daos/PostDao.java
+++ src/main/java/kr/megaptera/jdbc/assignment/daos/PostDao.java
@@ -1,4 +1,7 @@
 package kr.megaptera.jdbc.assignment.daos;
 
+import kr.megaptera.jdbc.assignment.models.posts.Post;
+
 public interface PostDao {
+    public void save(Post post);
 }
